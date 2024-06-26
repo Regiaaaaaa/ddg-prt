0f8559996fac9665fea3c35caf59c0c939c4f852
Regiaaaaaa <gheizarafashaquille@gmail.com>
2024-06-26 11:03:39 +0700

    first init github

---
+++ ddg.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>porto.g</title>
+    <link rel="stylesheet" href="ddg.css">
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+  <body>
+    <nav>
+      <div class="logo">portofo<span>lio</span></div>
+      <div class="nav-menu">
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Services</a>
+        <a href="">Skills</a>
+        <a href="">Contact</a>
+      </div>
+      <button>Download cv</button>
+    </nav>
+    <main>
+      <div class="hero">
+        <p>Haloo, saya</p>
+        <h1>Rafa <span>gheiza</span></h1>
+        <h3>Saya adalah Web designer</h3>
+        <div class="newslatter">
+          <form >
+            <input type="email" name="email" id="mail" placeholder="enteryouremail">
+            <input type="submit" name="submit" value="let's start">
+          </form>
+        </div>
+        </div>
+      </div>
+    </main>
+    <div class="about">
+      <div class="main">
+      <img src="aset/WhatsApp Image 2024-05-07 at 14.15.42_30c5552a.jpg" alt="">
+      <div class="about-text">
+      <h1>About Me</h1>
+      <h3>Developer <span>& designer</span></h3>
+      <p>hello, perkenalkan kami dari tim GZ Association, tim web designer yang bekerja di bidang teknologi yang dipimpin oleh mahasiswa yaitu saya M Rafa Gheiza Shaquille, saya siswa SMK Taruna Bhakti jurusan rekayasa perangkat lunak, saya lahir pada tahun 2008 1 April</p>
+      <button>Yuks ngobrol</button>
+    </div>
+    </div>
+    </div>
+    <div class="skill">
+      <h1>Skill</h1>
+      <div class="card-skill">
+        <div class="card-icon">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><g fill="none"><rect width="256" height="256" fill="#f0db4f" rx="60"/><path fill="#323330" d="m67.312 213.932l19.59-11.856c3.78 6.701 7.218 12.371 15.465 12.371c7.905 0 12.889-3.092 12.889-15.12v-81.798h24.058v82.138c0 24.917-14.606 36.259-35.916 36.259c-19.245 0-30.416-9.967-36.087-21.996m85.07-2.576l19.588-11.341c5.157 8.421 11.859 14.607 23.715 14.607c9.969 0 16.325-4.984 16.325-11.858c0-8.248-6.53-11.17-17.528-15.98l-6.013-2.579c-17.357-7.388-28.871-16.668-28.871-36.258c0-18.044 13.748-31.792 35.229-31.792c15.294 0 26.292 5.328 34.196 19.247l-18.731 12.029c-4.125-7.389-8.591-10.31-15.465-10.31c-7.046 0-11.514 4.468-11.514 10.31c0 7.217 4.468 10.139 14.778 14.608l6.014 2.577c20.449 8.765 31.963 17.699 31.963 37.804c0 21.654-17.012 33.51-39.867 33.51c-22.339 0-36.774-10.654-43.819-24.574"/></g></svg>
+        </div>
+        <div class="card-desk">
+          <h2>Javascript</h2>
+          <p>Saya bisa menggunakan js dan sudah beberapa project saya lakuka</p>
+          <button style="margin-top: 70px;">yuks ngobrol</button>
+        </div>
+      </div>
+      <div class="card-skill">
+        <div class="card-icon" style="width: 40px; height: 40px;">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 384"><path fill="#0acf83" d="M64 384c35.328 0 64-28.672 64-64v-64H64c-35.328 0-64 28.672-64 64s28.672 64 64 64"/><path fill="#a259ff" d="M0 192c0-35.328 28.672-64 64-64h64v128H64c-35.328 0-64-28.672-64-64"/><path fill="#f24e1e" d="M0 64C0 28.672 28.672 0 64 0h64v128H64C28.672 128 0 99.328 0 64"/><path fill="#ff7262" d="M128 0h64c35.328 0 64 28.672 64 64s-28.672 64-64 64h-64z"/><path fill="#1abcfe" d="M256 192c0 35.328-28.672 64-64 64s-64-28.672-64-64s28.672-64 64-64s64 28.672 64 64"/></svg>
+        </div>
+        <div class="card-desk" >
+          <h2>Figma</h2>
+          <p>Saya Bisa Menggunakan Figma Dengan lumayan fasih, Saya bisa membuat tampilan Web/FrontEnd untuk Mobile Maupun PC/Laptop</p>
+          <button style="margin-top: 50px;">yuks ngobrol</button>
+        </div>
+      </div>
+      <div class="card-skill">
+        <div class="card-icon">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path fill="#e44f26" d="M5.902 27.201L3.655 2h24.69l-2.25 25.197L15.985 30z"/><path fill="#f1662a" d="m16 27.858l8.17-2.265l1.922-21.532H16z"/><path fill="#ebebeb" d="M16 13.407h-4.09l-.282-3.165H16V7.151H8.25l.074.83l.759 8.517H16zm0 8.027l-.014.004l-3.442-.929l-.22-2.465H9.221l.433 4.852l6.332 1.758l.014-.004z"/><path fill="#fff" d="M15.989 13.407v3.091h3.806l-.358 4.009l-3.448.93v3.216l6.337-1.757l.046-.522l.726-8.137l.076-.83zm0-6.256v3.091h7.466l.062-.694l.141-1.567l.074-.83z"/></svg>
+        </div>
+        <div class="card-desk">
+          <h2>Html</h2>
+          <p>Saya sudah Fasih dengan HTML, Saya bisa menggunakan HTMl dan sudah beberapa project dan Web yang sudsh saya buat</p>
+          <button>yuks ngobrol</button>
+        </div>
+      </div>
+    </div>
+    <footer>
+      <div class="iklan">
+        <p>Mari lihat web yang lebih menarik dan indah</p>
+        <button>Hire Me</button>
+      </div>
+      <div class="footer-social">
+        <p>GZ Asociation</p>
+        <p>Selengkapnya, Yuk tersambung dengan media sosial kami </p>
+        <div class="footer-icon">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M14.2 2.875A4.625 4.625 0 0 0 9.575 7.5v2.575H7.1c-.124 0-.225.1-.225.225v3.4c0 .124.1.225.225.225h2.475V20.9c0 .124.1.225.225.225h3.4c.124 0 .225-.1.225-.225v-6.975h2.497c.103 0 .193-.07.218-.17l.85-3.4a.225.225 0 0 0-.218-.28h-3.347V7.5a.775.775 0 0 1 .775-.775h2.6c.124 0 .225-.1.225-.225V3.1c0-.124-.1-.225-.225-.225z"/></svg>
+      
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-width="1.5"><path stroke-linecap="round" stroke-linejoin="round" d="M12 16a4 4 0 1 0 0-8a4 4 0 0 0 0 8"/><path d="M3 16V8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5Z"/><path stroke-linecap="round" stroke-linejoin="round" d="m17.5 6.51l.01-.011"/></g></svg>
+    
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" d="M21.7 8.037a4.26 4.26 0 0 0-.789-1.964a2.84 2.84 0 0 0-1.984-.839c-2.767-.2-6.926-.2-6.926-.2s-4.157 0-6.928.2a2.84 2.84 0 0 0-1.983.839a4.2 4.2 0 0 0-.79 1.965a30 30 0 0 0-.2 3.206v1.5a30 30 0 0 0 .2 3.206c.094.712.364 1.39.784 1.972c.604.536 1.38.837 2.187.848c1.583.151 6.731.2 6.731.2s4.161 0 6.928-.2a2.84 2.84 0 0 0 1.985-.84a4.3 4.3 0 0 0 .787-1.965a30 30 0 0 0 .2-3.206v-1.516a31 31 0 0 0-.202-3.206m-11.692 6.554v-5.62l5.4 2.819z" clip-rule="evenodd"/></svg>
+        </div>
+        <p style="color: brown;">©️GZ Asc</p>
+      </div>
+    </footer>
+  </body>
+</html>
